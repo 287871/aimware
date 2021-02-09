c0a35e63cecdc76fd8bf71322c1ab3089bbb950e
287871 <66677129+287871@users.noreply.github.com>
2021-02-10 03:59:07 +0800

    Update Picture Circle - local.lua

--- Picture Circle - local.lua
+++ Picture Circle - local.lua
@@ -1,5 +1,7 @@
---Working on aimware
---by qi
+--[[
+Working on aimware picture circle
+by qi
+]]
 
 --gui
 local X, Y = draw.GetScreenSize()
@@ -12,7 +14,8 @@ local Avatars_Circle_Clr2 = gui.ColorPicker(Avatars_Circle_Checkbox, "clr2", "cl
 Avatars_Circle_X:SetInvisible(true)
 Avatars_Circle_Y:SetInvisible(true)
 
---var and DecodeJPEG
+--var and jpg
+local renderer = {}
 local tX, tY, offsetX, offsetY, _drag
 local MENU = gui.Reference("MENU")
 local font = draw.CreateFont('Verdana', 13.5, 11.5)
@@ -20,7 +23,7 @@ local font2 = draw.CreateFont('Verdana', 12, 11.5)
 local avatarsdata = file.Open("picture/1.jpg", "r")
 local avatars = avatarsdata:Read()
 avatarsdata:Close()
-local RGBA, width, height = common.DecodeJPEG(avatars);
+local RGBA, width, height = common.DecodeJPEG(avatars)
 local texture = draw.CreateTexture(RGBA, width, height)
 
 --function
@@ -55,43 +58,189 @@ local function drag_menu(x, y, w, h)
     return tX, tY
 end
 
---Gradient shadow
-local function gradientH(x1, y1, x2, y2,col1, left)
-    local w = x2 - x1
-    local h = y2 - y1
- 
-    for i = 0, w do
-        local a = (i / w) * col1[4]
-        local r, g, b = col1[1], col1[2], col1[3]
-        draw.Color(r,g,b, a)
-        if left then
-            draw.FilledRect(x1 + i, y1, x1 + i + 1, y1 + h)
-        else
-            draw.FilledRect(x1 + w - i, y1, x1 + w - i + 1, y1 + h)
+--renderer text
+renderer.text = function(x, y, clr, shadow, string, font, flags)
+
+    local alpha = 255
+    local textW, textH = draw.GetTextSize(string)
+
+    if font ~= nil then
+        draw.SetFont(font)
+    end
+
+    if clr[4] ~= nil then
+        alpha = clr[4]
+    end
+
+    if flags == "c" then
+        x = x - (textW / 2)
+    elseif flags == "l" then
+        x = x - textW
+    elseif flags == "r" then
+        x = x + textW
+    end
+
+    if shadow == 1 or true then
+        draw.Color(0, 0, 0, alpha)
+        draw.Text(x + 1, y + 1, string)
+    end
+
+    draw.Color(clr[1], clr[2], clr[3], alpha)
+	draw.Text(x, y, string)
+    alpha = nil
+    x, y = nil
+end
+
+--renderer rectangle
+renderer.rectangle = function(x, y, w, h, clr, fill, radius)
+
+    local alpha = 255
+
+    if clr[4] ~= nil then
+        alpha = clr[4]
+    end
+
+    draw.Color(clr[1], clr[2], clr[3], alpha)
+
+    if fill then
+        draw.FilledRect(x, y, x + w, y + h)
+    else
+        draw.OutlinedRect(x, y, x + w, y + h)
+    end
+
+    if fill == "shadow" then
+        draw.ShadowRect(x, y, x + w, y + h, radius)
+    end
+
+    alpha = nil
+end
+
+--renderer roundedrect
+renderer.roundedrect = function(x, y, w, h, fill, radius, clr, tl, tr, bl, br)
+
+    local alpha = 255
+
+    if clr[4] ~= nil then
+        alpha = clr[4]
+    end
+
+    draw.Color(clr[1], clr[2], clr[3], alpha)
+    if fill then
+        draw.RoundedRectFill(x, y, x + w, y + h, radius, tl, tr, bl, br)
+    else
+        draw.RoundedRect(x, y, x + w, y + h, radius, tl, tr, bl, br)
+    end
+    alpha = nil
+end
+
+--renderer circle
+renderer.circle = function(x, y, radius, clr, fill)
+
+    local alpha = 255
+
+    if clr[4] ~= nil then
+        alpha = clr[4]
+    end
+
+    draw.Color(clr[1], clr[2], clr[3], alpha)
+    if fill then
+        draw.FilledCircle(x, y, radius)
+    else
+        draw.OutlinedCircle(x, y, radius)
+    end
+    alpha = nil
+end
+
+--renderer gradient
+renderer.gradient = function(x, y, w, h, clr, clr1, vertical)
+    
+    local r, g, b, a = clr1[1], clr1[2], clr1[3], clr1[4]
+	local r1, g1, b1, a1 = clr[1], clr[2], clr[3], clr[4]
+
+    if a and a1 == nil then
+        a, a1 = 255, 255
+    end
+
+    if vertical then
+        if clr1[4] ~= 0 then
+            if a1 and a ~= 255 then
+                for i = 0, w do
+                    local a2 = i / w * a1
+                    renderer.rectangle(x, y + w - i, w, 1, {r1, g1, b1, a2}, true)
+                end
+            else
+                renderer.rectangle(x, y, w, h, {r1, g1, b1, a1}, true)
+            end
+        end
+        if a2 ~= 0 then
+            for i = 0, h do
+                local a2 = i / h * a
+                renderer.rectangle(x, y + i, w, 1, {r, g, b, a2}, true)
+            end
+        end
+    else
+        if clr[4] ~= 0 then
+            if a1 and a ~= 255 then
+                for i = 0, w do
+                    local a2 = i / w * a1
+                    renderer.rectangle(x + w - i, y, 1, h, {r1, g1, b1, a2}, true)
+                end
+            else
+                renderer.rectangle(x, y, w, h, {r1, g1, b1, a1}, true)
+            end
+        end
+        if a2 ~= 0 then
+            for i = 0, w do
+                local a2 = i / w * a
+                renderer.rectangle(x + i, y, 1, h, {r, g, b, a2}, true)
+            end
         end
     end
+    a, a1 = nil, nil
 end
 
---alpha
-local function alpha_stop(val, min, max)
-    if val < min then return min end
-    if val > max then return max end
-    return val
+--renderer circle_smooth
+renderer.circle_smooth = function(x, y, radius, clr)
+    local alpha = 255
+
+    if clr[4] ~= nil then
+        alpha = clr[4]
+    end
+
+    draw.Color(clr[1], clr[2], clr[3], alpha)
+    for i = 0, radius * 0.2 do
+        local a2 = i / radius * alpha
+        renderer.circle(x, y, radius * 0.95 + i * 0.2, {clr[1], clr[2], clr[3], a2})
+        renderer.circle(x, y, radius - i * 0.2, {clr[1], clr[2], clr[3], a2})
+    end
+    alpha = nil
+
 end
 
---Circular
-local function Circular(x, y, start, radius, number, thickness)
+--renderer circle_ring
+renderer.circle_ring = function(x, y, start, radius, number, thickness, clr, number2)
 
     if thickness > radius then
         thickness = radius
     end
+    local alpha = 255
+
+    if clr[4] ~= nil then
+        alpha = clr[4]
+    end
+    local _number2 = 1
+    if number2 ~= nil then
+        _number2 = number2
+    end
+
+    draw.Color(clr[1], clr[2], clr[3], alpha)
 
     for steps = start, number + start - 1, 1 do   
 
-        local sin_cur = math.sin(math.rad(steps))
-        local sin_old = math.sin(math.rad(steps - 1))
-        local cos_cur = math.cos(math.rad(steps))
-        local cos_old = math.cos(math.rad(steps - 1))
+        local sin_cur = math.sin(math.rad((steps * _number2) + (45 * _number2)))
+        local sin_old = math.sin(math.rad((steps * _number2) + (45 * _number2) - (2 * _number2)))
+        local cos_cur = math.cos(math.rad((steps * _number2) + (45 * _number2)))
+        local cos_old = math.cos(math.rad((steps * _number2) + (45 * _number2) - (2 * _number2)))
         local cur_point = nil
         local old_point = nil
         local cur_point = {x + sin_cur * radius, y + cos_cur * radius}    
@@ -105,7 +254,17 @@ local function Circular(x, y, start, radius, number, thickness)
         draw.Triangle(cur_point2[1], cur_point2[2], old_point2[1], old_point2[2], cur_point[1], cur_point[2])    
     
     end
+    _number2 = nil
+    alpha = nil
+end
 
+--renderer end
+
+--alpha
+local function alpha_stop(val, min, max)
+    if val < min then return min end
+    if val > max then return max end
+    return val
 end
 
 --On draw
@@ -122,38 +281,17 @@ local function Information()
 
     local x, y = drag_menu(tX, tY, 100, 100)
     local x, y = x + 50, y + 50
-    local lp = entities.GetLocalPlayer()
-    if lp ~= nil then
+
+    if entities.GetLocalPlayer() ~= nil then
+
         --Health
-        local r, g, b, a = 34, 34, 34, 255
-        gradientH(x - 130, y, x, y - 23,{ r, g, b, a * 0.6 }, true)
-        gradientH(x - 125, y, x, y - 23,{ r, g, b, a * 0.8 }, true)
-        gradientH(x - 120, y, x, y - 23,{ r, g, b, a }, true)
-        gradientH(x - 115, y, x, y - 23,{ r, g, b, a }, true)
-        gradientH(x - 112, y, x, y - 23,{ r, g, b, a }, true)
-        gradientH(x - 110, y, x, y - 23,{ r, g, b, a }, true)
-        gradientH(x - 108, y, x, y - 23,{ r, g, b, a }, true)
-        gradientH(x - 106, y, x, y - 23,{ r, g, b, a }, true)
+        renderer.gradient(x - 110, y - 23, 60, 23, {34, 34, 34, 0},{34, 34, 34, 255})
+
         --Money
-        local r, g, b, a = 40, 40, 40, 255
-        gradientH(x, y, x + 170, y + 20,{ r, g, b, a * 0.6 }, false)
-        gradientH(x, y, x + 165, y + 20,{ r, g, b, a * 0.8 }, false)
-        gradientH(x, y, x + 165, y + 20,{ r, g, b, a }, false)
-        gradientH(x, y, x + 160, y + 20,{ r, g, b, a }, false)
-        gradientH(x, y, x + 155, y + 20,{ r, g, b, a }, false)
-        gradientH(x, y, x + 152, y + 20,{ r, g, b, a }, false)
-        gradientH(x, y, x + 150, y + 20,{ r, g, b, a }, false)
-        gradientH(x, y, x + 148, y + 20,{ r, g, b, a }, false)
-        gradientH(x, y, x + 146, y + 20,{ r, g, b, a }, false)
+        renderer.gradient(x + 50, y, 120, 20, {40, 40, 40, 255},{40, 40, 40, 0})
+
         --Misc
-        local r, g, b, a = 34, 34, 34, 255
-        gradientH(x + 42, y - 10, x + 220, y - 35,{ r, g, b, a * 0.8 }, false)
-        gradientH(x + 42, y - 10, x + 215, y - 35,{ r, g, b, a * 0.6 }, false)
-        gradientH(x + 42, y - 10, x + 210, y - 35,{ r, g, b, a }, false)
-        gradientH(x + 42, y - 10, x + 208, y - 35,{ r, g, b, a }, false)
-        gradientH(x + 42, y - 10, x + 206, y - 35,{ r, g, b, a }, false)
-        gradientH(x + 42, y - 10, x + 204, y - 35,{ r, g, b, a }, false)
-        gradientH(x + 42, y - 10, x + 202, y - 35,{ r, g, b, a }, false)
+        renderer.gradient(x + 60, y - 35, 170, 25, {34, 34, 34, 255},{34, 34, 34, 0})
 
     end
 
@@ -165,45 +303,46 @@ local function animation()
     local lp = entities.GetLocalPlayer()
     local x, y = drag_menu(tX, tY, 100, 100)
     local x, y = x + 50, y + 50
-    local fade_factor = ((1.0 / 0.15) * globals.FrameTime()) * 65
+    local fade_factor = ((1.0 / 0.15) * globals.FrameTime()) * 30
     if lp ~= nil then
+
         local health = lp:GetHealth()
-        if healthA == nil then
-            healthA = {alpha = 360}
+
+        if healthAlpha == nil then
+            healthAlpha =  180
         end
-        if health * 3.6 ~= 360 then -- alpha animation
-            healthA.alpha = alpha_stop(healthA.alpha - fade_factor, health * 3.6, 360)
+        if health * 1.8 ~= 180 then -- alpha animation
+            healthAlpha = alpha_stop(healthAlpha - fade_factor, health * 1.8, 180)
         else
-            healthA.alpha = alpha_stop(healthA.alpha + fade_factor, 0, health * 3.6)
+            healthAlpha = alpha_stop(healthAlpha + fade_factor, 0, health * 1.8)
         end
 
         if health > 75 then --Multiple blood colors
-            draw.Color(134, 200, 134, 200)
+            r, g, b, a = 134, 200, 134, 200
         elseif health > 60 then
-            draw.Color(171, 151, 106, 200) 
+            r, g, b, a = 171, 151, 106, 200
         elseif health > 40 then
-            draw.Color(190, 90, 90, 200)  
+            r, g, b, a = 190, 90, 90, 200
         elseif health > 20 then
-            draw.Color(200, 0, 60, 200) 
+            r, g, b, a = 200, 0, 60, 200
         elseif health >= 0 then
-            draw.Color(255, 0, 0, 200) 
+            r, g, b, a = 255, 0, 0, 200
         end
-        
-        Circular(x, y, 95 - healthA.alpha, 57, healthA.alpha, 1)
+        renderer.circle_ring(x, y, 4 - healthAlpha, 57, healthAlpha, 2, {r, g, b, a}, 2)
 
         if health > 75 then --Multiple blood colors  For smooth edges
-            draw.Color(134, 200, 134, 150)
+            r, g, b, a = 134, 200, 134, 50
         elseif health > 60 then
-            draw.Color(171, 151, 106, 130) 
+            r, g, b, a = 171, 151, 106, 50
         elseif health > 40 then
-            draw.Color(190, 90, 90, 130)  
+            r, g, b, a = 190, 90, 90, 50
         elseif health > 20 then
-            draw.Color(200, 0, 60, 130) 
+            r, g, b, a = 200, 0, 60, 50
         elseif health >= 0 then
-            draw.Color(255, 0, 0, 130) 
+            r, g, b, a = 255, 0, 0, 50
         end
-        Circular(x, y, 95 - healthA.alpha, 57.5, healthA.alpha, 1)
-
+        renderer.circle_ring(x, y, 4 - healthAlpha, 57.5, healthAlpha, 1, {r, g, b, a}, 2)
+        
     end
 
 end
@@ -216,26 +355,6 @@ local function Circle()
     local lp = entities.GetLocalPlayer()
 
     if lp ~= nil then
-        -- To make the edges smooth
-        local r , g , b , a = 36, 36, 36, 255
-        draw.Color(r, g, b, a)
-        draw.FilledCircle(x, y, 55)
-        draw.Color(r, g, b, a * 0.8)
-        draw.FilledCircle(x, y, 55.1)
-        draw.FilledCircle(x, y, 55.2)
-        draw.Color(r, g, b, a * 0.6)
-        draw.FilledCircle(x, y, 55.3)
-        draw.FilledCircle(x, y, 55.4)
-        draw.FilledCircle(x, y, 55.5)
-        draw.Color(r, g, b, a * 0.4)
-        draw.FilledCircle(x, y, 55.6)
-        draw.FilledCircle(x, y, 55.7)
-        draw.FilledCircle(x, y, 55.8)
-        draw.FilledCircle(x, y, 55.9)
-        draw.Color(r, g, b, a * 0.2)
-        draw.FilledCircle(x, y, 56)
-        draw.FilledCircle(x, y, 56.1)
-        draw.FilledCircle(x, y, 56.2)
 
         --picture
         local r, g, b, a = 255, 255, 255, 255
@@ -244,44 +363,18 @@ local function Circle()
         draw.FilledRect(x - 39, y - 39, width - 145 + x, height - 145 + y)
 
         --Number plate
-        local r, g, b, a = 80, 163, 248, 50
-        draw.Color(r, g, b, a)
-        draw.FilledCircle(x, y + 37, 14)
-        draw.Color(r, g, b, a * 0.8)
-        draw.FilledCircle(x, y + 37, 14.1)
-        draw.Color(r, g, b, a * 0.6)
-        draw.FilledCircle(x, y + 37, 14.2)
-        draw.FilledCircle(x, y + 37, 14.3)
-        draw.Color(r, g, b, a * 0.4)
-        draw.FilledCircle(x, y + 37, 14.4)
-        draw.FilledCircle(x, y + 37, 14.5)
-        draw.FilledCircle(x, y + 37, 14.6)
-        draw.Color(r, g, b, a * 0.2)
-        draw.FilledCircle(x, y + 37, 14.7)
-        draw.FilledCircle(x, y + 37, 14.8)
-        draw.FilledCircle(x, y + 37, 14.9)
-        draw.FilledCircle(x, y + 37, 15)
+        renderer.circle(x, y + 37, 15, {80, 163, 248, 150},true)
+        renderer.circle_smooth(x, y + 37, 15, {80, 163, 248, 255})
 
-        --draw Circular
-        draw.Color(34, 34, 34, 255)
-        Circular(x, y, 1, 55, 360, 16)
-        --For smoothing
-        local r, g, b, a = 34, 34, 34, 255
-        draw.Color(r, g, b, a * 0.8)
-        draw.OutlinedCircle(x, y, 39.9)
-        draw.Color(r, g, b, a * 0.6)
-        draw.OutlinedCircle(x, y, 39.8)
-        draw.OutlinedCircle(x, y, 39.7)
-        draw.Color(r, g, b, a * 0.4)
-        draw.OutlinedCircle(x, y, 39.6)
-        draw.OutlinedCircle(x, y, 39.5)
-        draw.OutlinedCircle(x, y, 39.4)
-        draw.Color(r, g, b, a * 0.2)
-        draw.OutlinedCircle(x, y, 39.3)
-        draw.OutlinedCircle(x, y, 39.2)
-        draw.OutlinedCircle(x, y, 39.1)
-        draw.OutlinedCircle(x, y, 39)
- 
+        --draw roundedrect
+        renderer.roundedrect(x - 50.5, y - 50.5, 101, 101, false, 50.5, {34, 34, 34, 255}, 50, 50,50, 50)
+        renderer.roundedrect(x - 46, y - 46, 92, 92, false, 46, {34, 34, 34, 255}, 50, 50,50, 50)
+        renderer.roundedrect(x - 44, y - 44, 88, 88, false, 44, {34, 34, 34, 255}, 50, 50,50, 50)
+        renderer.roundedrect(x - 55, y - 55, 110, 110, false, 55, {34, 34, 34, 255}, 50, 50,50, 50)
+
+        --draw circle_smooth
+        renderer.circle_smooth(x, y, 56, {34, 34, 34, 255})
+        
     end
 
 end
@@ -292,27 +385,26 @@ local function cover()
     local lp = entities.GetLocalPlayer()
     local x, y = drag_menu(tX, tY, 100, 100)
     local x, y = x + 50 , y + 50
-    local fade_factor = ((1.0 / 0.15) * globals.FrameTime()) * 65
+    local fade_factor = ((1.0 / 0.15) * globals.FrameTime()) * 30
     if lp ~= nil then
 
         local armor = lp:GetProp("m_ArmorValue")
-        if armorA == nil then
-            armorA = {alpha = 360}
+
+        if armorAlpha == nil then
+            armorAlpha = 180
         end
-        if armor * 3.6 ~= 360 then-- alpha animation
-            armorA.alpha = alpha_stop(armorA.alpha - fade_factor, armor * 3.6, 360)
+        if armor * 1.8 ~= 180 then-- alpha animation
+            armorAlpha = alpha_stop(armorAlpha - fade_factor, armor * 1.8, 180)
         else
-            armorA.alpha = alpha_stop(armorA.alpha + fade_factor, 0, armor * 3.6)
+            armorAlpha = alpha_stop(armorAlpha + fade_factor, 0, armor * 1.8)
         end
 
-        draw.Color(80, 163, 248, 200) --I don't want to repeat that, but for the sake of smoothness
-        Circular(x, y - 0.5, 1, 41, armorA.alpha, 0.8)
-        draw.Color(80, 163, 248, 150)
-        Circular(x, y - 0.5, 1, 41.2, armorA.alpha, 0.8)
-        draw.Color(80, 163, 248, 150)
-        Circular(x, y - 0.5, 1, 40.8, armorA.alpha, 0.8)
+        renderer.circle_ring(x, y - 0.5, -45, 41, armorAlpha, 1, {80, 163, 248, 200}, 2)
+        renderer.circle_ring(x, y - 0.5, -45, 41.5, armorAlpha, 0.8, {80, 163, 248, 50}, 2)
+        renderer.circle_ring(x, y - 0.5, -45, 40.5, armorAlpha, 0.8, {80, 163, 248, 50}, 2)
 
     end
+
 end
 
 --Health, Money Text
@@ -323,37 +415,26 @@ local function Text()
     local x, y = x + 50, y + 50
     local indexlp = client.GetLocalPlayerIndex()
     local userName = client.GetPlayerNameByIndex(indexlp)
+    local r, g, b, a = Avatars_Circle_Clr:GetValue()
+    local r2, g2, b2, a2 = Avatars_Circle_Clr2:GetValue()
 
     if lp ~= nil then
 
         local health = lp:GetHealth()
         local money = lp:GetProp("m_iAccount")
         local teamnumber = lp:GetTeamNumber()
-        local x1, y1 = draw.GetTextSize(teamnumber)
 
         if health > 75 then --Multiple blood colors
-            draw.Color(134, 200, 134, 255)
+            hpr, hpg, hpb = 134, 200, 134
         elseif health > 60 then
-            draw.Color(171, 151, 106, 255) 
+            hpr, hpg, hpb = 171, 151, 106 
         elseif health > 40 then
-            draw.Color(190, 90, 90, 255)  
+            hpr, hpg, hpb = 190, 90, 90 
         elseif health > 20 then
-            draw.Color(200, 0, 60, 255) 
+            hpr, hpg, hpb = 200, 0, 60
         elseif health >= 0 then
-            draw.Color(255, 0, 0, 255) 
+            hpr, hpg, hpb = 255, 0, 0
         end
-        draw.SetFont( font2 )
-        draw.TextShadow(x - 92, y - 16, health.." hp") --Health
-
-
-        draw.Color(Avatars_Circle_Clr:GetValue())
-        draw.TextShadow(x + 65, y + 5, "Money ● ")
-
-        draw.Color(Avatars_Circle_Clr2:GetValue())
-        draw.TextShadow(x + 110, y + 5, "$"..money) --Money
-
-        draw.SetFont( font )
-        draw.TextShadow(x - x1 + 4, y + 27, teamnumber) --Team number  
 
         if (engine.GetServerIP() == "loopback") then -- Server
             server = "localhost"
@@ -368,14 +449,19 @@ local function Text()
             userName = userName.."..."
         end
 
-        draw.SetFont(font2)
-        draw.TextShadow(x + 65, y - 27, server.."  Hello "..userName) --Server UserName
+        renderer.text(x - 52, y - 16, {hpr, hpg, hpb}, true, health.." hp", font2, "l") --Health
+        renderer.text(x + 65, y + 5, {r, g, b, a}, true, "Money ● ", font2) --Money
+        renderer.text(x + 110, y + 5, {r2, g2, b2, a2}, true, "$"..money, font2) --Money
+        renderer.text(x - 5, y + 27, {r2, g2, b2, a2}, true, teamnumber, font) --Team number  
+        renderer.text(x + 65, y - 27, {r2, g2, b2, a2}, true, server.."  Hello "..userName, font2) --misc
 
+        renderer.roundedrect(x + 40, y - 35, 20, 25, true, 10, {34, 34, 34, 255})
     end
+
 end
 
 --Callbacks
-callbacks.Register("Draw", function()
+callbacks.Register("Draw", "avatars_circle", function()
     if Avatars_Circle_Checkbox:GetValue() then
         PositionSave()
         Information()
@@ -385,4 +471,5 @@ callbacks.Register("Draw", function()
         Text()
     end
 end)
+
 --end
