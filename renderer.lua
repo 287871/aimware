45aaf36ea2a4006c621902e4d98d93a8fa9909f1
287871 <66677129+287871@users.noreply.github.com>
2021-03-30 19:18:35 +0800

    Update renderer.lua

--- renderer.lua
+++ renderer.lua
@@ -14,7 +14,7 @@ end
 
 --[[
 
-syntax: renderer.text(x, y, r, g, b, a, flags, max_width, ...)
+syntax: renderer.text(x, y, r, g, b, a, string, shadow)
 
 x - Screen coordinate
 
@@ -30,36 +30,61 @@ a - Alpha (0-255)
 
 string - Text that will be drawn
 
-shadow - Shadow through true, no shadow through false
-
+mode - Show shadows "s", no shadows ""
 
 This can only be called from the paint callback.
 
 ]]
-function renderer.text(x, y, r, g, b, a, string, shadow)
+function renderer.text(x, y, r, g, b, a, string, mode)
     local x, y = math_modf(x), math_modf(y)
 
     renderer_assert(type(x and y) == "number", 3, "'" .. tostring(x and y) .. "' must be a number.")
 
     renderer_assert(type(r and g and b and a) == "number", 3, "'" .. tostring(r and g and b and a) .. "' must be a number.")
 
-    renderer_assert(type(string) == "string", 3, "'" .. tostring(string) .. "' must be a string.")
+    renderer_assert(type(string) == "string" or "number", 3, "'" .. tostring(string) .. "' must be a string.")
 
-    renderer_assert(type(shadow) == "boolean", 3, "'" .. tostring(shadow) .. "' must be a boolean.")
+    renderer_assert(type(mode) == "string", 3, "'" .. tostring(mode) .. "' must be a 's' or ''.")
 
-    if shadow then
+    if mode == "s" then
         draw.Color(0, 0, 0, a)
         draw.Text(x + 1, y + 1, string)
         draw.Color(r, g, b, a)
         draw.Text(x, y, string)
-    else
+    elseif mode == "" then
         draw.Color(r, g, b, a)
         draw.Text(x, y, string)
     end
+    draw.Color(255, 255, 255, 255)
 end
+
 --[[
 
-syntax: renderer_gradient(x, y, w, h, r1, g1, b1, a1, r2, g2, b2, a2, ltr)
+syntax: renderer.measure_text(string, font)
+
+string - Text that will be measured
+
+font - Text font to be measured
+
+Returns width, height. This can only be called from the paint callback.
+
+]]
+local default_font = draw.CreateFont("Verdana", 15)
+function renderer.measure_text(string, font)
+    renderer_assert(type(string) == "string", 3, "'" .. tostring(string) .. "' must be a string.")
+
+    if font then
+        renderer_assert(type(font) == "userdata", 3, "Invalid font '" .. tostring(font) .. "'.")
+        draw.SetFont(font)
+    end
+    local width, height = draw.GetTextSize(string)
+    draw.SetFont(default_font)
+    return width, height
+end
+
+--[[
+
+syntax: renderer.gradient(x, y, w, h, r1, g1, b1, a1, r2, g2, b2, a2, ltr)
 
 x - Screen coordinate
 
@@ -103,7 +128,7 @@ function renderer.gradient(x, y, w, h, r1, g1, b1, a1, r2, g2, b2, a2, ltr)
         if a1 ~= 0 then
             if a1 and a2 ~= 255 then
                 for i = 0, w do
-                    draw.Color(r1, g1, b1, i / h * a1)
+                    draw.Color(r1, g1, b1, i / w * a1)
                     draw.FilledRect(x + w - i, y, x + w - i + 1, y + h)
                 end
             else
@@ -131,11 +156,12 @@ function renderer.gradient(x, y, w, h, r1, g1, b1, a1, r2, g2, b2, a2, ltr)
         end
         if a2 ~= 0 then
             for i = 0, h do
-                draw.Color(r2, g2, b2, i / w * a2)
+                draw.Color(r2, g2, b2, i / h * a2)
                 draw.FilledRect(x, y + i, x + w, y + i + 1)
             end
         end
     end
+    draw.Color(255, 255, 255, 255)
 end
 
 --[[
@@ -162,10 +188,12 @@ percentage - Must be within [0.0-1.0]. 1.0 is a full circle, 0.5 is a half circl
 
 thickness - Thickness of the outline in pixels.
 
+cycle - Number of cycles 1 = 360.
+
 This can only be called from the paint callback.
 
 ]]
-function renderer.circle_outline(x, y, r, g, b, a, radius, start_degrees, percentage, thickness)
+function renderer.circle_outline(x, y, r, g, b, a, radius, start_degrees, percentage, thickness, cycle)
     renderer_assert(type(x and y) == "number", 3, "'" .. tostring(x and y) .. "' must be a number.")
 
     renderer_assert(type(r and g and b and a) == "number", 3, "'" .. tostring(r and g and b and a) .. "' must be a number.")
@@ -178,17 +206,20 @@ function renderer.circle_outline(x, y, r, g, b, a, radius, start_degrees, percen
 
     renderer_assert(type(thickness) == "number", 3, "'" .. tostring(thickness) .. "' must be a number.")
 
+    renderer_assert(type(cycle) == "number", 3, "'" .. tostring(cycle) .. "' must be a number.")
+
     local cur_point
     local old_point
     local cur_point2
     local old_point2
     draw.Color(r, g, b, a)
-    for steps = start_degrees, (percentage * 360 - 1) + start_degrees, 1 do
-        local steps = steps + 90
-        local sin_cur = math_sin(math_rad(steps))
-        local sin_old = math_sin(math_rad(steps - 1))
-        local cos_cur = math_cos(math_rad(steps))
-        local cos_old = math_cos(math_rad(steps - 1))
+    for steps = start_degrees, (percentage * 360) + (start_degrees) - 1, 1 do
+        local steps = steps + 91.5
+        local sin_cur = math_sin(math_rad(steps * cycle))
+        local sin_old = math_sin(math_rad(steps * cycle - (cycle * 2)))
+        local cos_cur = math_cos(math_rad(steps * cycle))
+        local cos_old = math_cos(math_rad(steps * cycle - (cycle * 2)))
+
         local cur_point = {x + sin_cur * radius, y + cos_cur * radius}
         local old_point = {x + sin_old * radius, y + cos_old * radius}
         local cur_point2 = {x + sin_cur * (radius - thickness), y + cos_cur * (radius - thickness)}
@@ -197,6 +228,41 @@ function renderer.circle_outline(x, y, r, g, b, a, radius, start_degrees, percen
         draw.Triangle(cur_point[1], cur_point[2], old_point[1], old_point[2], old_point2[1], old_point2[2])
         draw.Triangle(cur_point2[1], cur_point2[2], old_point2[1], old_point2[2], cur_point[1], cur_point[2])
     end
+    draw.Color(255, 255, 255, 255)
+end
+
+function renderer.load_svg(contents, scale)
+    return draw.CreateTexture(common.RasterizeSVG(contents, scale or 1))
+end
+
+function renderer.load_png(contents)
+    return draw.CreateTexture(common.DecodePNG(contents))
+end
+
+function renderer.load_jpg(contents)
+    return draw.CreateTexture(common.DecodeJPEG(contents))
+end
+
+--[[
+
+syntax: renderer.texture(texture, x, y, w, h)
+
+texture - Loaded texture
+
+x - X screen coordinate
+
+y - Y screen coordinate
+
+w - Width
+
+h - Height
+
+This can only be called from the paint callback.
+]]
+function renderer.texture(texture, x, y, w, h)
+    draw.SetTexture(texture)
+    draw.FilledRect(x, y, x + w, y + h)
+    draw.SetTexture(nil)
 end
 
 return renderer
