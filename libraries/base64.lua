6ab21b117d94cb31f90a79ea92ed8b1a92201110
287871 <66677129+287871@users.noreply.github.com>
2021-04-14 17:42:22 +0800

    Add files via upload

---
+++ libraries/base64.lua
@@ -0,0 +1,87 @@
+-- Pasted from http://lua-users.org/wiki/BaseSixtyFour
+
+-- Download and run the library. Put this in your script if you wish to use
+
+--[=====[
+local function require(filename, url)
+    local filename = filename .. ".lua"
+
+    local function http_write(body)
+        file.Write(filename, body)
+    end
+
+    local module = RunScript(filename) or http.Get(url, http_write)
+
+    return module or error("unable to load module " .. filename, 2)
+end
+
+local a =
+    require(
+    "libraries/base64",
+    "https://aimware28.coding.net/p/coding-code-guide/d/aimware/git/raw/master/libraries/base64.lua?download=false"
+)
+--]=====]
+
+--base64.enc(data)
+--base64.dec(data)
+
+local b = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/" -- You will need this for encoding/decoding
+base64 = {}
+-- encoding
+
+function base64.enc(data)
+    return ((data:gsub(
+        ".",
+        function(x)
+            local r, b = "", x:byte()
+            for i = 8, 1, -1 do
+                r = r .. (b % 2 ^ i - b % 2 ^ (i - 1) > 0 and "1" or "0")
+            end
+            return r
+        end
+    ) .. "0000"):gsub(
+        "%d%d%d?%d?%d?%d?",
+        function(x)
+            if (#x < 6) then
+                return ""
+            end
+            local c = 0
+            for i = 1, 6 do
+                c = c + (x:sub(i, i) == "1" and 2 ^ (6 - i) or 0)
+            end
+            return b:sub(c + 1, c + 1)
+        end
+    ) .. ({"", "==", "="})[#data % 3 + 1])
+end
+
+-- decoding
+function base64.dec(data)
+    data = string.gsub(data, "[^" .. b .. "=]", "")
+    return (data:gsub(
+        ".",
+        function(x)
+            if (x == "=") then
+                return ""
+            end
+            local r, f = "", (b:find(x) - 1)
+            for i = 6, 1, -1 do
+                r = r .. (f % 2 ^ i - f % 2 ^ (i - 1) > 0 and "1" or "0")
+            end
+            return r
+        end
+    ):gsub(
+        "%d%d%d?%d?%d?%d?%d?%d?",
+        function(x)
+            if (#x ~= 8) then
+                return ""
+            end
+            local c = 0
+            for i = 1, 8 do
+                c = c + (x:sub(i, i) == "1" and 2 ^ (8 - i) or 0)
+            end
+            return string.char(c)
+        end
+    ))
+end
+
+return base64
